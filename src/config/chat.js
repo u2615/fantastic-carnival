cff43126547f2f56429dd9b88eb61f02ff8dca96
antoine <antoine.serveaux+dev@gmail.com>
2023-03-28 18:14:15 +0200

    remove js files from src

--- src/config/chat.js
+++
@@ -1,8 +0,0 @@
-export const ROLES = {
-    default: 'you are a helpful ai assistant. Format your answers in html.',
-};
-export const chatConfig = {
-    model: 'gtp-3.5-turbo',
-    temperature: 0.8,
-    role: 'default',
-};
--- src/config/default.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    homeserverUrl: 'http://monolith:8008',
-    dataPath: 'storage',
-    accessToken: '/run/secrets/botAccessToken',
-    APIKeyPath: '/run/secrets/openAIKey',
-    cacheFilePath: 'chatCache.json',
-    organization: 'laniakea'
-};
